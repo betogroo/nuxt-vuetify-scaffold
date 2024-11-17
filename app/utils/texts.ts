00d6a9cc01588665ef5d14a6a3b710daef9eedad
betogroo <luizhumberto@gmail.com>
2024-11-17 14:46:39 -0300

    feat: create text utils

---
+++ app/utils/texts.ts
@@ -0,0 +1,5 @@
+export const MANDATORY_FIELD = 'Campo Obrigatório'
+export const TWO_LETTERS_MINIMUM_NAME =
+  'Parte do nome deve conter no mínimo duas letras'
+export const MUST_INCLUDE_FIRST_AND_LAST_NAME =
+  'Deve conter pelo menos o nome e sobrenome'
