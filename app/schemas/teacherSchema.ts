60602934b8334acb1b34d96e03352c24a3133215
betogroo <luizhumberto@gmail.com>
2024-11-17 14:20:56 -0300

    fix(teacher): remove unused code

--- app/schemas/teacherSchema.ts
+++ app/schemas/teacherSchema.ts
@@ -1,6 +1,5 @@
 import { toTypedSchema } from '@vee-validate/zod'
 import { z } from 'zod'
-import { jsonSchema } from '~/schemas'
 
 export const nameSchema = z
   .string()
@@ -21,24 +20,18 @@ export const teacherRowSchema = z.object({
   id: z.string(),
   created_at: z.string(),
   name: z.string(),
-  availabilities: jsonSchema.nullable(),
-  subjects: jsonSchema.nullable(),
 })
 
 export const teacherInsertSchema = z.object({
   id: z.string().optional(),
   created_at: z.string().optional(),
   name: nameSchema,
-  availabilities: jsonSchema.optional().nullable(),
-  subjects: jsonSchema.optional().nullable(),
 })
 
 export const teacherUpdateSchema = z.object({
   id: z.string().optional(),
   created_at: z.string().optional(),
   name: nameSchema.optional(),
-  availabilities: jsonSchema.optional().nullable(),
-  subjects: jsonSchema.optional().nullable(),
 })
 
 export const validateTeacher = toTypedSchema(teacherInsertSchema)
