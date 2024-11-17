5a84361672bfa4d378d59d201b587328cc4c10ac
betogroo <luizhumberto@gmail.com>
2024-11-17 12:55:06 -0300

    feat: create add teacher

--- app/pages/schedule/teacher/index.vue
+++ app/pages/schedule/teacher/index.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
   import type { TeacherInsert } from '~/types'
+  const { addTeacher, isPending } = useTeacher()
+  const { showToast, handleError } = useHelpers()
 
-  const handleSubmit = (values: TeacherInsert) => {
-    console.log(values)
+  const handleSubmit = async (teacher: TeacherInsert) => {
+    try {
+      const newTeacher = await addTeacher(teacher)
+      if (!newTeacher) throw new Error('Erro ao Adicionar Professor')
+      showToast(
+        'success',
+        `Professor ${newTeacher.name} cadastrado com sucesso`,
+      )
+    } catch (err) {
+      const e = err as Error
+      const error = handleError(e)
+      showToast('error', error.message)
+      console.error(error)
+    }
   }
 </script>
 
 <template>
   <div>
     <div>Professores</div>
-    <FormTeacher @on-submit="handleSubmit" />
+    <FormTeacher
+      :is-pending="isPending.isLoading && isPending.action === 'addTeacher'"
+      @on-submit="handleSubmit"
+    />
   </div>
 </template>
