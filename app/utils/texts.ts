b85fd39ec9c131ca601bef728236bc0a717d5557
betogroo <luizhumberto@gmail.com>
2024-12-09 14:53:48 -0300

    feat: add new utils text

--- app/utils/texts.ts
+++ app/utils/texts.ts
@@ -3,3 +3,8 @@ export const TWO_LETTERS_MINIMUM_NAME =
   'Parte do nome deve conter no mínimo duas letras'
 export const MUST_INCLUDE_FIRST_AND_LAST_NAME =
   'Deve conter pelo menos o nome e sobrenome'
+
+export const NO_SPACES = 'O campo não pode conter espaços'
+
+export const MIN_LENGTH = (value: number) =>
+  `O campo deve conter pelo menos ${value} caracteres`
