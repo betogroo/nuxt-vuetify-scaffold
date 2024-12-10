fbff9097d09913bc6d492be039322b9f75f4c218
betogroo <luizhumberto@gmail.com>
2024-12-10 14:14:13 -0300

    feat: create generic update

---
+++ app/composables/useGenericUpdate.ts
@@ -0,0 +1,35 @@
+import type { ZodSchema } from 'zod'
+import type { Database, Tables } from '~/types'
+
+const useGenericUpdate = <UpdateType>(
+  tableName: Tables,
+
+  schema: ZodSchema,
+) => {
+  const supabase = useSupabaseClient<Database>()
+  const { isPending: updatePending, setPendingState } = useHelpers()
+  const { validateWithSchema } = useSchema()
+
+  const update = async (
+    id: string | number,
+    data: UpdateType,
+    pendingRow?: string | number | undefined,
+  ) => {
+    await setPendingState(
+      async () => {
+        const parsedData = validateWithSchema(data, schema)
+        const { error } = await supabase
+          .from(tableName)
+          .update(parsedData)
+          .eq('id', id)
+        if (error) throw error
+      },
+      tableName,
+      { itemId: pendingRow, delay: 5000 },
+    )
+  }
+
+  return { update, updatePending }
+}
+
+export default useGenericUpdate
