83b4c93896822b0906688590b20aee9d3e33e90d
betogroo <luizhumberto@gmail.com>
2024-12-11 16:39:01 -0300

    feat: create purchase page and form

---
+++ app/pages/uge/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  definePageMeta({
+    showInNavBar: true,
+    requiresAuth: true,
+    title: 'UGE',
+    order: 2,
+  })
+</script>
+
+<template>
+  <div>UGE</div>
+  <div><FormPurchaseDemand /></div>
+</template>
