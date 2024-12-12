113f4cba66d9225cbc81793a8fbad38304cecd5b
betogroo <luizhumberto@gmail.com>
2024-12-12 14:00:19 -0300

    feat: create purchasing demand form

--- app/pages/uge/index.vue
+++ app/pages/uge/index.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
+  import type { PurchasingDemandInsert } from '~/types'
+  const { showToast, handleError } = useHelpers()
+
+  const { insertPurchasingDemand, purchasingPending } = usePurchasingDemand()
+
   definePageMeta({
     showInNavBar: true,
     requiresAuth: true,
     title: 'UGE',
     order: 2,
   })
+
+  const submitForm = async (data: PurchasingDemandInsert) => {
+    try {
+      await insertPurchasingDemand(data)
+      showToast('success', 'Dados inseridos com sucesso')
+    } catch (error) {
+      handleError(error)
+    }
+  }
 </script>
 
 <template>
-  <div>UGE</div>
-  <div><FormPurchaseDemand /></div>
+  <div>
+    <FormPurchaseDemand
+      :is-pending="purchasingPending.isLoading"
+      @on-submit="submitForm"
+    />
+  </div>
 </template>
