9bc341182dea00093e91ae8e827910468a247ab5
betogroo <luizhumberto@gmail.com>
2024-12-16 16:52:12 -0300

    put purchase demand form on modal

---
+++ app/pages/uge/demand/index.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+  import type { PurchasingDemandInsert, PurchasingDemand } from '~/types'
+  const { handleError } = useHelpers()
+
+  const { insertPurchasingDemand, purchasingPending } = usePurchasingDemand()
+  const purchaseDemandModal = ref(false)
+  const closeModal = () => {
+    purchaseDemandModal.value = false
+  }
+  const openModal = () => {
+    purchaseDemandModal.value = true
+  }
+
+  definePageMeta({
+    showInNavBar: false,
+    requiresAuth: true,
+    title: 'UGE - Demandas',
+  })
+
+  const submitForm = async (
+    data: PurchasingDemandInsert,
+    onSuccess: (id: string | number) => void,
+    onError: (message: string) => void,
+  ) => {
+    try {
+      const insertedData: PurchasingDemand = await insertPurchasingDemand(data)
+      if (!insertedData) throw Error('Erro ao tentar inserir a demanda')
+      onSuccess(insertedData.id)
+      closeModal()
+    } catch (error) {
+      onError(`Erro ao tentar inserir a demanda, ${handleError(error).message}`)
+    }
+  }
+</script>
+
+<template>
+  <v-container class="fill-height flex-column justify-space-between align-end">
+    <div class="w-100">
+      <AppModal
+        v-model="purchaseDemandModal"
+        title="Cadastrar Processo"
+      >
+        <FormPurchaseDemand
+          :is-pending="purchasingPending.isLoading"
+          @on-submit="
+            (values, onSuccess, onError) =>
+              submitForm(values, onSuccess, onError)
+          "
+        />
+      </AppModal>
+    </div>
+    <v-fab
+      absolute
+      class="mr-4"
+      color="green"
+      :icon="iconOutline.plus"
+      @click="openModal"
+    />
+  </v-container>
+</template>
--- app/pages/uge/index.vue
+++ app/pages/uge/index.vue
@@ -1,38 +1,14 @@
 <script setup lang="ts">
-  import type { PurchasingDemandInsert, PurchasingDemand } from '~/types'
-  const { handleError } = useHelpers()
-
-  const { insertPurchasingDemand, purchasingPending } = usePurchasingDemand()
-
   definePageMeta({
     showInNavBar: true,
     requiresAuth: true,
     title: 'UGE',
     order: 2,
   })
-
-  const submitForm = async (
-    data: PurchasingDemandInsert,
-    onSuccess: (id: string | number) => void,
-    onError: (message: string) => void,
-  ) => {
-    try {
-      const insertedData: PurchasingDemand = await insertPurchasingDemand(data)
-      if (!insertedData) throw Error('Erro ao tentar inserir a demanda')
-      onSuccess(insertedData.id)
-    } catch (error) {
-      onError(`Erro ao tentar inserir a demanda, ${handleError(error).message}`)
-    }
-  }
 </script>
 
 <template>
   <div>
-    <FormPurchaseDemand
-      :is-pending="purchasingPending.isLoading"
-      @on-submit="
-        (values, onSuccess, onError) => submitForm(values, onSuccess, onError)
-      "
-    />
+    <router-link to="/uge/demand">Processos</router-link>
   </div>
 </template>
