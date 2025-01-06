f62a90d599e95e35d45b66e90f6e10982daa3578
betogroo <luizhumberto@gmail.com>
2025-01-06 14:28:15 -0300

    create use validate param composable

--- app/pages/uge/demand/[id].vue
+++ app/pages/uge/demand/[id].vue
@@ -1,73 +1,7 @@
 <script setup lang="ts">
-  const { fetch, profiles, availableSupportTeam } = useSupportTeamStoreStore()
-  const { params } = useRoute()
-  const { getPurchasingDemandById, detailedPurchasingRow, fetchAgents } =
-    useDetailedPurchasing()
-  const {
-    getAvailableSupportTeam,
-    getDesignedSupportTeam,
-    availableSupportTeamProfile,
-    designedSupportTeamProfile,
-
-    isPending: supportTeamIsPending,
-  } = useSupportTeam()
-
-  const id = Array.isArray(params.id) ? params.id[0] : params.id
-
-  onMounted(async () => {
-    if (id) await getPurchasingDemandById(id)
-    await fetchAgents()
-    await getAvailableSupportTeam(+id!)
-    await getDesignedSupportTeam(+id!)
-    await fetch()
-  })
-
-  const {
-    id: process,
-    description,
-    ptres_number,
-    contracting_agent,
-    contracting_agent_id,
-  } = detailedPurchasingRow.value
-
-  const coisos = computed(() => {
-    return availableSupportTeam(
-      contracting_agent_id,
-      detailedPurchasingRow.value,
-    )
-  })
+  const { id } = useValidateParam()
 </script>
 
 <template>
-  <div>
-    <h1>Processo número {{ process }}</h1>
-    <h2>Descrição {{ description }}</h2>
-    <h2>PTRES {{ ptres_number }}</h2>
-    <h2>Agente de Contratação {{ contracting_agent }}</h2>
-    <h2>
-      Equipe de Apoio:
-      <v-list-item
-        v-for="teamProfile in coisos"
-        :key="teamProfile.id"
-      >
-        {{ teamProfile.name }}
-      </v-list-item>
-    </h2>
-    <h2>
-      Funcionários Disponíveis:
-      <template
-        v-if="supportTeamIsPending.action === 'get-available-support_team'"
-        >Loading
-      </template>
-      <template v-else>
-        <v-list-item
-          v-for="item in coisos"
-          :key="item.id"
-        >
-          {{ item.name }} <v-btn @click="console.log(item.id)">+</v-btn>
-        </v-list-item>
-      </template>
-    </h2>
-    <v-divider />
-  </div>
+  <div>Aqui nemanda numero {{ id }}</div>
 </template>
