8f20dfb8637b39f78b493f37872dbf0f5ef5c521
betogroo <luizhumberto@gmail.com>
2025-01-07 10:40:07 -0300

    refactor form schema

--- app/schemas/index.ts
+++ app/schemas/index.ts
@@ -1,14 +1,16 @@
+export * from './userSchema'
+export * from './profileSchema'
+
 export * from './app/color'
 export * from './app/dropdown'
 export * from './app/navbar'
 export * from './app/table'
 
-export * from './userSchema'
-export * from './profileSchema'
-
 export * from './class/row'
 export * from './class/insert'
 export * from './class/update'
+
+export * from './form/form'
 
 export * from './documentDemandSchema'
 export * from './purchasingDemandSchema'
@@ -16,5 +18,4 @@ export * from './helpersSchema'
 export * from './teacherSchema'
 export * from './subjectSchema'
 export * from './timeSlotSchema'
-export * from './formSchema'
 export * from './supportTeamSchema'
