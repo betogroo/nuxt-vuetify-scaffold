222e7da3311fc2e5c24b5b1154a097da9b7a9eb6
betogroo <luizhumberto@gmail.com>
2025-01-14 15:14:06 -0300

    feat: add data/time format functions

--- app/utils/format.ts
+++ app/utils/format.ts
@@ -2,3 +2,8 @@ export const capitalize = (s: string) =>
   s.charAt(0).toLocaleUpperCase() + s.slice(1)
 
 export const isDevelopment = () => process.env.NODE_ENV !== 'production'
+
+export const timestampToDate = (date: string) =>
+  new Date(date).toLocaleDateString('pt-BR')
+
+export const getYear = (date: string) => new Date(date).getUTCFullYear()
