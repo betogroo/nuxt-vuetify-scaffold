bd9cced6a0db3d0ad034d14316ea057bb087ee8b
betogroo <luizhumberto@gmail.com>
2025-01-22 17:02:13 -0300

    refactor: add action slot on uge card

--- app/components/uge/card.vue
+++ app/components/uge/card.vue
@@ -6,9 +6,19 @@
 </script>
 
 <template>
-  <v-card
-    :title
-    variant="outlined"
+  <v-card variant="outlined"
     ><slot />
+    <template #title>
+      <v-row align="center">
+        <v-col :cols="!$slots.action ? 12 : 9">{{ title }}</v-col>
+        <v-col
+          v-if="$slots.action"
+          class="text-right"
+          cols="3"
+        >
+          <slot name="action" />
+        </v-col>
+      </v-row>
+    </template>
   </v-card>
 </template>
