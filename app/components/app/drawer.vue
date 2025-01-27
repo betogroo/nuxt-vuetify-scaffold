1c55f671e8ddb24ace7425a2c4f6fec5e7e4d3ec
betogroo <luizhumberto@gmail.com>
2025-01-27 10:31:23 -0300

    fix: hydrattion error

--- app/components/app/drawer.vue
+++ app/components/app/drawer.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+  const { smAndDown } = useDisplay()
   const isActive = defineModel<boolean>()
 </script>
 
 <template>
   <v-navigation-drawer
     v-model="isActive"
-    :location="$vuetify.display.mobile ? 'bottom' : 'right'"
+    :location="smAndDown ? 'bottom' : 'right'"
+    :style="{ zIndex: 1004 }"
     temporary
     width="500"
   >
