3992526ac5dba966f19e509a4af56bc713619588
betogroo <luizhumberto@gmail.com>
2025-01-29 15:53:04 -0300

    eat: add product details

--- app/components/product/list.vue
+++ app/components/product/list.vue
@@ -16,5 +16,33 @@
 </script>
 
 <template>
-  <div v-if="productsOnDemand?.length">{{ productsOnDemand }}</div>
+  <div
+    v-if="productsOnDemand?.length"
+    class="d-flex justify-space-around flex-wrap"
+  >
+    <v-card
+      v-for="product in productsOnDemand"
+      :key="product.id"
+      class="ma-2"
+      color="primary"
+      height="135"
+      variant="tonal"
+      width="240"
+    >
+      <template #title
+        ><span class="text-body-2">{{
+          `Item ${product.item_number} - 
+          ${product.name}`
+        }}</span></template
+      >
+      <v-row
+        class="text-caption"
+        no-gutters
+      >
+        <v-col cols="6"> Quantidade {{ product.quantity }}</v-col>
+        <v-col>Valor Unitário {{ product.price }}</v-col>
+        <v-col cols="12">Valor Total {{ product.total_price }}</v-col>
+      </v-row>
+    </v-card>
+  </div>
 </template>
