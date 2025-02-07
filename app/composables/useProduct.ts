fa8a6315a15bfea527e4c5963792e8320c4a8edb
betogroo <luizhumberto@gmail.com>
2025-02-07 14:50:23 -0300

    refactor: move tableColumn to table component

--- app/composables/useProduct.ts
+++ app/composables/useProduct.ts
@@ -1,35 +1,14 @@
-import type { TableColumn } from '~/types'
-
 const useProduct = () => {
   const products = useProducts()
   const productOnDemand = useProductOnDemand()
   const productClasses = useProductClasses()
   const productExpenseCategories = useProductExpenseCategories()
 
-  const tableColumns: TableColumn[] = [
-    {
-      key: 'cat_mat',
-      title: 'Cat Mat',
-    },
-    {
-      key: 'name',
-      title: 'Descrição',
-    },
-    {
-      key: 'product_class_id',
-      title: 'Classe',
-    },
-    {
-      key: 'expense_category_id',
-      title: 'Natureza',
-    },
-  ]
   return {
     ...products,
     ...productOnDemand,
     ...productClasses,
     ...productExpenseCategories,
-    tableColumns,
   }
 }
 
