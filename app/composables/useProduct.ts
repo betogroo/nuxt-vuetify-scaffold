6fa25542ccb99c14d00b6805b844259b93dc8e03
betogroo <luizhumberto@gmail.com>
2025-02-11 08:41:07 -0300

    feat create get product by id

--- app/composables/useProduct.ts
+++ app/composables/useProduct.ts
@@ -1,14 +1,23 @@
+import { productRowSchema } from '~/schemas'
+import type { ProductRow } from '~/types'
 const useProduct = () => {
   const products = useProducts()
   const productOnDemand = useProductOnDemand()
   const productClasses = useProductClasses()
   const productExpenseCategories = useProductExpenseCategories()
 
+  const { data: product, getById: getProduct } = useGenericGet<ProductRow>(
+    'products',
+    productRowSchema,
+  )
+
   return {
     ...products,
     ...productOnDemand,
     ...productClasses,
     ...productExpenseCategories,
+    product,
+    getProduct,
   }
 }
 
--- app/pages/uge/products/[id].vue
+++ app/pages/uge/products/[id].vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
   const id = useValidateParam().id
+
+  const { product, getProduct } = useProduct()
+  onBeforeMount(async () => {
+    await getProduct(id!)
+  })
 </script>
 
 <template>
   <div>Produto {{ id }}</div>
+  <div>{{ product }}</div>
 </template>
