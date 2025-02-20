c8eb7a9b46631d277844b475315e3608f86f1cc3
betogroo <luizhumberto@gmail.com>
2025-02-20 09:23:59 -0300

    feat: create product classe list

--- app/pages/uge/products/classes/index.vue
+++ app/pages/uge/products/classes/index.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const router = useRouter()
+  const { productClasses, fetchProductClasses } = useProductClasses()
+
+  const goTo = (id: number) => {
+    if (id !== undefined && id !== null)
+      router.push({ name: 'uge-products-classes-id', params: { id } })
+    else console.warn('Código de Classe errado', id)
+  }
+
+  onMounted(async () => {
+    try {
+      await fetchProductClasses({ column: 'id' })
+    } catch (error) {
+      console.error(error)
+    }
+  })
+</script>
 
 <template>
-  <div>Classes</div>
+  <v-container>
+    <h1>Classes de Produtos</h1>
+    <v-list
+      density="compact"
+      item-title="name"
+      item-value="id"
+      :items="productClasses"
+      nav
+      @click:select="(item: any) => goTo(item.id)"
+    >
+      <template #prepend="{ item }">({{ item.id }})</template>
+    </v-list>
+  </v-container>
 </template>
