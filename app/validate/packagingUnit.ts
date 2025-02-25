2639df6ad3d9d009d4942685be9a3ff5f72706b0
betogroo <luizhumberto@gmail.com>
2025-02-25 11:02:33 -0300

    chore: create validate insert

--- app/validate/packagingUnit.ts
+++ app/validate/packagingUnit.ts
@@ -1,4 +1,7 @@
 import { toTypedSchema } from '@vee-validate/zod'
-import { packagingUnitRowSchema } from '~/schemas'
+import { packagingUnitRowSchema, packagingUnitInsertSchema } from '~/schemas'
 
 export const validatePackagingUnitRow = toTypedSchema(packagingUnitRowSchema)
+export const validatePackagingUnitInsert = toTypedSchema(
+  packagingUnitInsertSchema,
+)
