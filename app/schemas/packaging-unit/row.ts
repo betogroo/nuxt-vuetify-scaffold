5918136cdb3d39de98506d14fdbb1710f76ddfc2
betogroo <luizhumberto@gmail.com>
2025-02-25 05:10:31 -0300

    fix: id type

--- app/schemas/packaging-unit/row.ts
+++ app/schemas/packaging-unit/row.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const packagingUnitRowSchema = z.object({
-  id: uuidSchema,
+  id: numberRequiredSchema,
   created_at: createdAtSchema.optional(),
   name: nameSchema,
   name_bec: nameSchema,
