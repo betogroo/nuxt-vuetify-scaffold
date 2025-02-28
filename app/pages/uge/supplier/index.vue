5407dba5e708c77eddd221a2b8cea5dc39686320
betogroo <luizhumberto@gmail.com>
2025-02-28 15:37:00 -0300

    feat: create supplier page

---
+++ app/pages/uge/supplier/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  definePageMeta({
+    showInNavBar: false,
+    requiresAuth: true,
+    title: 'Fornecedores',
+  })
+</script>
+
+<template>
+  <v-container>
+    <h1>Fornecedores</h1>
+  </v-container>
+</template>
