a472a64032d89f9634850614a88a46c5dd2083e1
betogroo <luizhumberto@gmail.com>
2025-03-07 16:21:33 -0300

    feat: create icon details

---
+++ app/components/app/icon/details/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import type { To } from '~/types'
+
+  interface Props {
+    to: To
+  }
+  defineProps<Props>()
+</script>
+
+<template>
+  <v-btn
+    density="compact"
+    :icon="iconOutline.details"
+    :ripple="false"
+    size="24"
+    :to="to"
+    variant="text"
+  />
+</template>
