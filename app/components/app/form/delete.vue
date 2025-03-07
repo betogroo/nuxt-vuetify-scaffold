d5d966b7e16e3a7d65acbd86365072924be9613a
betogroo <luizhumberto@gmail.com>
2025-03-07 09:33:51 -0300

    feat: create app form delete

---
+++ app/components/app/form/delete.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+  const $emit = defineEmits<{
+    'on-cancel': []
+    'on-confirm': [id: string | number]
+  }>()
+
+  const onConfirm = (id: string | number) => {
+    $emit('on-confirm', id)
+  }
+
+  const isActive = defineModel<boolean>()
+</script>
+
+<template>
+  <AppModal
+    v-model="isActive"
+    max-width="300"
+    title="Confirmar a exclusão?"
+    @on-close="$emit('on-cancel')"
+  >
+    <v-card>
+      <v-card-text>Tem certeza que deseja excluir </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn
+          min-width="140"
+          variant="outlined"
+          @click="$emit('on-cancel')"
+          >Cancelar</v-btn
+        >
+        <v-btn
+          color="error"
+          min-width="140"
+          variant="tonal"
+          @click="onConfirm"
+          >Excluir</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </AppModal>
+</template>
