8c1df89b5bca65f3f8f344cf4d723724641b4da8
betogroo <luizhumberto@gmail.com>
2025-03-07 09:34:23 -0300

    feat: create icon delete

---
+++ app/components/app/icon/delete/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  const $emit = defineEmits<{
+    'open-modal': []
+  }>()
+</script>
+
+<template>
+  <div>
+    <v-btn
+      color="error"
+      density="compact"
+      :icon="iconOutline.trash"
+      size="24"
+      variant="text"
+      @click="$emit('open-modal')"
+    />
+    <slot />
+  </div>
+</template>
