6a8788db92b00137bc4e17a3f72628bab7e244b2
betogroo <luizhumberto@gmail.com>
2025-04-01 10:38:37 -0300

    feat: add insert modal

--- app/pages/voting/index.vue
+++ app/pages/voting/index.vue
@@ -8,6 +8,33 @@
     order: 4,
   })
 
+  const {
+    openModal: openConfirmDeleteModal,
+    isActive: isConfirmDeleteModalActive,
+    props: confirmDeleteModalProps,
+    closeModal: closeConfirmDeleteModal,
+  } = useModal()
+
+  const {
+    openModal: openInsertVotingFormModal,
+    isActive: isInsertVotingFormModalActive,
+    props: insertVotingFormModalProps,
+    closeModal: closeInsertVotingFormModal,
+  } = useModal()
+
+  const handleConfirmDeleteModal = (id: string | number) => {
+    openConfirmDeleteModal({ id })
+    console.log('Open the modal', id)
+  }
+  const handleConfirmDeleteVoting = () => {
+    console.log(confirmDeleteModalProps.value.id)
+    closeConfirmDeleteModal()
+  }
+
+  const handleOpenInsertVotingFormModal = () => {
+    openInsertVotingFormModal({ title: 'Inserir Eleição' })
+  }
+
   const elections: Election[] = [
     {
       id: 1,
@@ -38,7 +65,23 @@
       Nesta página teremos uma tabela com as votações em curso. Cada uma terá o
       link para ir aos detalhes para a votação correspondente.
       {{ elections }}
-      <VotingTableElection :items="elections" />
+      <VotingTableElection
+        :items="elections"
+        @delete-click="(id) => handleConfirmDeleteModal(id)"
+      />
     </AppCard>
+    <AppModalWithDeleteAction
+      :model-value="isConfirmDeleteModalActive"
+      @on-cancel="closeConfirmDeleteModal"
+      @on-confirm="handleConfirmDeleteVoting"
+    />
+    <AppModalWithFabActivator
+      v-model="isInsertVotingFormModalActive"
+      :title="insertVotingFormModalProps.title || ''"
+      @open-modal="handleOpenInsertVotingFormModal"
+    >
+      Aqui vai o formulário
+      <v-btn @click="closeInsertVotingFormModal">Cancelar</v-btn>
+    </AppModalWithFabActivator>
   </v-container>
 </template>
