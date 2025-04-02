6ab31699910644c5f314c930b0f296d49c4a0d3e
betogroo <luizhumberto@gmail.com>
2025-04-02 10:12:40 -0300

    refactor: schema and types

--- app/schemas/election/index.ts
+++ app/schemas/election/index.ts
@@ -1,7 +1,34 @@
 import { z } from 'zod'
 
 export const electionRowSchema = z.object({
-  id: z.number(),
-  name: z.string(),
-  date: z.string(),
+  id: uuidSchema,
+  created_at: createdAtSchema,
+  updated_at: createdAtSchema,
+  name: nameSchema,
+  date: z.string().date(),
 })
+export const electionRowsSchema = z.array(electionRowSchema)
+
+/* election: {
+  Row: {
+    created_at: string
+    date: string
+    id: string
+    name: string
+    updated_at: string
+  }
+  Insert: {
+    created_at?: string
+    date: string
+    id?: string
+    name: string
+    updated_at?: string
+  }
+  Update: {
+    created_at?: string
+    date?: string
+    id?: string
+    name?: string
+    updated_at?: string
+  }
+ */
--- app/types/election.ts
+++ app/types/election.ts
@@ -1,4 +1,4 @@
 import type { z } from 'zod'
 import type { electionRowSchema } from '~/schemas/election'
 
-export type Election = z.infer<typeof electionRowSchema>
+export type ElectionRow = z.infer<typeof electionRowSchema>
