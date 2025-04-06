c85cadcfdbe20ea680749aebe1e737b0848c8e6f
betogroo <luizhumberto@gmail.com>
2025-04-06 19:36:31 -0300

    feat: add realtime on ballot box insert

--- app/composables/useBallotBox.ts
+++ app/composables/useBallotBox.ts
@@ -1,9 +1,27 @@
-import { ballotBoxRowsSchema } from '~/schemas'
-import type { BallotBoxRow } from '~/types'
+import { ballotBoxRowSchema, ballotBoxRowsSchema } from '~/schemas'
+import type { BallotBoxRow, Database } from '~/types'
 
 const useBallotBox = () => {
+  const supabase = useSupabaseClient<Database>()
   const { getWithFilters: getBallotBoxesByElectionId, data: ballotBoxes } =
     useGenericGet<BallotBoxRow[]>('ballot_box', ballotBoxRowsSchema)
+
+  const channel = supabase.channel('custom-update-channel')
+  channel
+    .on(
+      'postgres_changes',
+      {
+        event: 'INSERT',
+        schema: 'public',
+        table: 'ballot_box',
+      },
+      (payload) => {
+        const newRecord = ballotBoxRowSchema.parse(payload.new)
+        ballotBoxes.value = [...(ballotBoxes.value || []), newRecord]
+        console.log(payload)
+      },
+    )
+    .subscribe()
   return { getBallotBoxesByElectionId, ballotBoxes }
 }
 
