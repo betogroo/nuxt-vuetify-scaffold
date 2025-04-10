3b402980b4305e1c15ef883f4855fc522e64bf95
betogroo <luizhumberto@gmail.com>
2025-04-10 16:25:19 -0300

    feat: add realtime update channel

--- app/composables/useBallotBox.ts
+++ app/composables/useBallotBox.ts
@@ -43,6 +43,27 @@ const useBallotBox = () => {
           ballotBoxes.value?.filter((item) => item.id !== payload.old.id) || []
       },
     )
+    .on(
+      'postgres_changes',
+      {
+        event: 'UPDATE',
+        schema: 'public',
+        table: 'ballot_box',
+      },
+      (payload) => {
+        if (ballotBoxes.value) {
+          console.log(payload.old, payload.new)
+          const newRecord = ballotBoxRowSchema.parse(payload.new)
+          const index = ballotBoxes.value.findIndex(
+            (item) => item.id === payload.old.id,
+          )
+          if (index !== -1 && ballotBoxes.value[index])
+            Object.assign(ballotBoxes.value[index], newRecord)
+
+          console.log(newRecord, index)
+        }
+      },
+    )
     .subscribe()
   return {
     getBallotBoxesByElectionId,
