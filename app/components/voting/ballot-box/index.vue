2f1d307fb74ec03e2f3609864b7f5b8f7cfa9f41
betogroo <luizhumberto@gmail.com>
2025-04-30 15:52:13 -0300

    refactor: set content prop to string array

--- app/components/voting/ballot-box/index.vue
+++ app/components/voting/ballot-box/index.vue
@@ -6,10 +6,10 @@
   }
   defineProps<Props>()
 
-  const content = ref<string>('')
+  const content = ref<string[]>([])
 
   const handleNumericButton = (value: number) => {
-    content.value += value
+    content.value.push(value.toString())
   }
 </script>
 
@@ -17,7 +17,10 @@
   <v-container>
     <div class="h1">Urna</div>
     <div>{{ ballotBox }}</div>
-    <VotingBallotBoxNumericDisplay :content="content" />
+    <VotingBallotBoxNumericDisplay
+      :content="content"
+      :length="3"
+    />
     <v-card>
       <v-sheet class="d-flex justify-center">
         <VotingBallotBoxNumericBtn
--- app/components/voting/ballot-box/numeric-display.vue
+++ app/components/voting/ballot-box/numeric-display.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
   interface Props {
-    content: string
+    content: string[]
     length?: number
   }
-  const props = withDefaults(defineProps<Props>(), {
+  withDefaults(defineProps<Props>(), {
     length: 3,
   })
-  const display = computed(() => props.content.split(''))
-  //const display = computed(() => props.content)
 </script>
 
 <template>
@@ -21,7 +19,7 @@
         height="50"
         width="50"
       >
-        {{ display[i] }}
+        {{ content[i] }}
       </v-sheet>
     </v-sheet>
   </v-container>
